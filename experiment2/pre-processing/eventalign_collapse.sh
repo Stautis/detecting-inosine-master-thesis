a47cc78bd0f2dd16d80ed5b85a76e31236864b7c
Stautis <thkrst@gmail.com>
2021-08-10 14:10:13 +0200

    Create eventalign_collapse.sh

---
+++ experiment2/pre-processing/eventalign_collapse.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+NanopolishComp Eventalign_collapse \
+		-i /export/valenfs/data/processed_data/MinION/inosine_project/20201016_max_DNA_Inosine/second_analysis/3_event_align/construct1/A_in_reference/A_ref_eventalign.tsv \
+		-o /export/valenfs/data/processed_data/MinION/inosine_project/20201016_max_DNA_Inosine/second_analysis/4_collapsed/construct1/A_in_reference/ \
+		-t 32
